ae8cb6723103f511e35534e536d027a14a672caa
0x-m1cro <0xm1cro@proton.me>
2023-04-23 22:30:26 +0500

    faisaax-default.tokenlist.json init

--- src/buildList.js
+++ src/buildList.js
@@ -10,13 +10,14 @@ const optimism = require("./tokens/optimism.json");
 const celo = require("./tokens/celo.json");
 const arbitrum = require("./tokens/arbitrum.json");
 const bnb = require("./tokens/bnb.json");
+const testnet = require("./tokens/testnet.json");
 
 const bridgeUtils = require('@uniswap/token-list-bridge-utils');
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   const l1List = {
-    name: "Uniswap Labs Default",
+    name: "FaisaaX Default",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -25,8 +26,8 @@ module.exports = function buildList() {
     },
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
-    keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai, ...optimism, ...celo, ...arbitrum, ...bnb]
+    keywords: ["faisaax", "default"],
+    tokens: [...mainnet, ...testnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai, ...optimism, ...celo, ...arbitrum, ...bnb]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
